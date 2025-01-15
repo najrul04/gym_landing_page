762bcc3cbc9fa2fe3ff0b4efc6e3971ff5119634
MD Najrul Islam Sumon <nazrul171992@gmail.com>
2025-01-15 19:36:53 +0600

    fixed-review-card-issue

--- gym_landing_page/src/components/ReviewCard.svelte
+++ gym_landing_page/src/components/ReviewCard.svelte
@@ -16,17 +16,17 @@
 >
     <div class="flex relative">
         <div
-            class={"absolute reounded-full bg-white aspect-square hidden md:grid place-items-center h-4 top-0 " +
+            class={"absolute rounded-full bg-white aspect-square hidden md:grid place-items-center h-4 top-0 " +
                 (index % 2 === 0
                     ? " right-0 -mr-8 translate-x-1/2"
                     : " left-0 -ml-8 -translate-x-1/2")}
         >
             <div
-                class="rounded-full bg-slate-950 aspect-square bg-slate-950 h-2"
-            />
+                class="rounded-full aspect-square bg-slate-950 h-2"
+            ></div>
         </div>
         <div class="flex items-center gap-2">
-            <i class="fa-solid fa-user" />
+            <i class="fa-solid fa-user"></i>
             <h3 class="text-lg sm:text-xl md:text-2xl">
                 {reviewItem.name}
             </h3>
